ea6aa487948ed4f7db7d92f5406fa111c06f954f
MegaMagnum <87041564+MegaMagnum@users.noreply.github.com>
2022-08-08 00:21:03 +0200

    Update 1.2 Colored messages!

--- src/main/java/me/megamagnum/extrasounds/eventMessages.java
+++ src/main/java/me/megamagnum/extrasounds/eventMessages.java
@@ -23,21 +23,36 @@ public class eventMessages implements Listener {
         if(config.getBoolean("ChatPing")){
         for (Player pinged : Bukkit.getOnlinePlayers()) {
             if(e.getMessage().toLowerCase().contains(pinged.getName().toLowerCase())) {
-
+                e.setCancelled(true);
                 if(config.getBoolean("Users." + pinged.getUniqueId() + "sounds")) {
+                    e.setCancelled(true);
+
                     sender.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You have pinged " + pinged.getName() + "!"));
                     pinged.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You have been pinged by " + sender.getName() + "!"));
-                    pinged.playSound(pinged.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 3F, 20F);
+                    pinged.playSound(pinged.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 3F, 0.8F);
+                    sender.playSound(sender.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 3F, 0.7F);
+
+                    for(Player everyone : Bukkit.getOnlinePlayers()){
+                        if(everyone.getUniqueId().equals(pinged.getUniqueId())){
+                            pinged.sendMessage("<"+ sender.getDisplayName()+"> " + ChatColor.RED+e.getMessage());
+
+                        } else if (everyone.getUniqueId().equals(sender.getUniqueId())) {
+                          sender.sendMessage( "<"+ sender.getDisplayName()+"> " + ChatColor.AQUA+e.getMessage());
+                        }else
+                            everyone.sendMessage("<"+ sender.getDisplayName()+"> " +e.getMessage());
+                    }
+                    }else{
+                        sender.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "The player you are trying to ping has his pings toggled off"));
                 }
-                else{
-                    sender.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "The player you are trying to ping has his pings toggled off"));
-                    sender.playSound(sender.getLocation(), Sound.ENTITY_VILLAGER_NO, 3F, 1F);
+
                 }
+
+
             }
             }
         }
     }
-}
+
 
 
 
